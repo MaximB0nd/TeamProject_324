7573d7c62eacaa10132a4ebc6eb736bc89aa5018
Luca324 <tanya.mosol@yandex.ru>
2025-10-28 10:39:53 +0300

    Mosolova

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,19 @@
+import allclasses.mosolova.Rectangle;
+
 public class Main {
     static void bondarevMethod() {
         System.out.println("asdasd");
     }
 
-    static void main() {
+    static void mosolovaMethod() {
+        Rectangle rect = new Rectangle(5, 10);
+        System.out.println("Прямоугольник 5x10:");
+        System.out.println("Площадь: " + rect.getArea());
+        System.out.println("Периметр: " + rect.getPerimeter());
+    }
+
+    public static void main(String[] args) {
         bondarevMethod();
+        mosolovaMethod();
     }
 }
