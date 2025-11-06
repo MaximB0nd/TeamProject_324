094d25ab6c68c67ed2b7fc30885249d926afdebc
Valaeva Irehe <valaevai@yandex.ru>
2025-11-06 16:35:31 +0300

    feat(valaeva): added the Cat and cat tests

--- src/main/java/allclasses/valaeva/Cat.java
+++ src/main/java/allclasses/valaeva/Cat.java
@@ -1,4 +1,61 @@
 package allclasses.valaeva;
 
+
 public class Cat {
+    protected String name;
+    protected int age;
+    protected double weight;
+    protected String color;
+
+
+    public Cat(String name, int age, double weight, String color) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+    public String getInfo() {
+        return "Кот: " + name + ", возраст: " + age + " лет, вес: " + weight + " кг, цвет: " + color;
+    }
+
+    // Простые методы
+    public String eat() {
+        return name + " кушает";
+    }
+
+    public String sleep() {
+        return name + " спит";
+    }
+
+
+    public void setWeight(double newWeight) {
+        this.weight = newWeight;
+    }
+
+    public boolean isFat() {
+        return weight > 6.0;
+    }
+
+    public String getColorInfo() {
+        return name + " имеет " + color + " цвет";
+    }
 }
--- src/test/java/valaevatests/cattests.java
+++ src/test/java/valaevatests/cattests.java
@@ -1,4 +1,69 @@
 package valaevatests;
 
+import allclasses.valaeva.Cat;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class cattests {
+
+    @Test
+    public void testCatCreation() {
+        Cat cat = new Cat("Барсик", 3, 4.5, "рыжий");
+
+        assertEquals("Барсик", cat.getName());
+        assertEquals(3, cat.getAge());
+        assertEquals(4.5, cat.getWeight(), 0.01);
+        assertEquals("рыжий", cat.getColor());
+    }
+
+    @Test
+    public void testGetInfo() {
+        Cat cat = new Cat("Мурзик", 2, 3.8, "черный");
+        String info = cat.getInfo();
+
+        assertEquals("Кот: Мурзик, возраст: 2 лет, вес: 3.8 кг, цвет: черный", info);
+    }
+
+    @Test
+    public void testEat() {
+        Cat cat = new Cat("Васька", 4, 5.2, "серый");
+        assertEquals("Васька кушает", cat.eat());
+    }
+
+    @Test
+    public void testSleep() {
+        Cat cat = new Cat("Пушок", 1, 2.5, "белый");
+        assertEquals("Пушок спит", cat.sleep());
+    }
+
+    @Test
+    public void testSetWeight() {
+        Cat cat = new Cat("Толстяк", 5, 6.0, "черный");
+        cat.setWeight(7.5);
+        assertEquals(7.5, cat.getWeight(), 0.01);
+    }
+
+    @Test
+    public void testIsFat() {
+        Cat fatCat = new Cat("Толстый", 4, 6.5, "серый");
+        Cat thinCat = new Cat("Худой", 2, 5.5, "рыжий");
+
+        assertTrue(fatCat.isFat());
+        assertFalse(thinCat.isFat());
+    }
+
+    @Test
+    public void testGetColorInfo() {
+        Cat cat = new Cat("Рыжик", 3, 4.1, "рыжий");
+        assertEquals("Рыжик имеет рыжий цвет", cat.getColorInfo());
+    }
+
+    @Test
+    public void testWeightBoundary() {
+        Cat boundaryCat = new Cat("Граничный", 3, 6.0, "серый");
+        Cat heavyCat = new Cat("Тяжелый", 4, 6.1, "рыжий");
+
+        assertFalse(boundaryCat.isFat());
+        assertTrue(heavyCat.isFat());
+    }
 }
