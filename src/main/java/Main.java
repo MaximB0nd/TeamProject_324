f8269ebe22cc7352ffa2fa22547f6d8e337086d8
simka <sonya@gmail.com>
2025-11-11 09:28:55 +0300

    feat: new class, methods in main and tests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import allclasses.prokofev.WalletProkofev;
 import java.math.BigDecimal;
 import allclasses.larin.Book;
 import allclasses.shulga.Triangle;
+import allclasses.simonenko.Fractions;
 
 public class Main {
     public static void bondarevMethod() {
@@ -77,12 +78,36 @@ public class Main {
         return;
     }
 
+    public static void SimonenkoMetod () {
+        // Создание дробей
+        Fractions frac1 = new Fractions(1, 2);
+        Fractions frac2 = new Fractions(3, 4);
+        Fractions frac3 = new Fractions(5);
+
+        System.out.println("Дробь 1: " + frac1);
+        System.out.println("Дробь 2: " + frac2);
+        System.out.println("Дробь 3: " + frac3);
+
+        // Сложение
+        Fractions sum = frac1.add(frac2);
+        System.out.println(frac1 + " + " + frac2 + " = " + sum);
+
+        // Вычитание
+        Fractions difference = frac1.subtract(frac2);
+        System.out.println(frac1 + " - " + frac2 + " = " + difference);
+
+        // Сокращение дробей
+        Fractions simplified = new Fractions(4, 8);
+        System.out.println("Сокращение 4/8: " + simplified);
+    }
+
     public static void main(String[] args) {
         bondarevMethod();
         BartasevichMethod();
         testWallet();
         larinMethod();
         shulgaMethods();
+        SimonenkoMetod();
     }
     
 }
