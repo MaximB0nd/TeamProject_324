e5e6144a667b7169471bedcfcc8801a4db1c931f
Skulyabina Margarita <leraandrita@gmail.com>
2025-12-01 12:16:29 +0300

    My student class and tests

---
+++ src/main/java/allclasses/skulyabina/student.java
@@ -0,0 +1,21 @@
+package allclasses.skulyabina;
+
+public class student {
+    private String name;
+    private int age;
+    private String group;
+
+    public student(String name, int age, String group) {
+        this.name = name;
+        this.age = age;
+        this.group = group;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student: " + name + ", Age: " + age + ", Group: " + group);
+    }
+
+    public void study() {
+        System.out.println(name + " is studying...");
+    }
+}
