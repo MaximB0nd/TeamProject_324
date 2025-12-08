21a8c8dc2b3bd9a730850d37d3329d6cc8d2dee2
MaximB0nd <sersim11@yandex.ru>
2025-12-08 10:54:54 +0300

    fix: ref valaeva method

--- src/test/java/valaevatests/Cattests.java
+++ src/test/java/valaevatests/Cattests.java
@@ -2,9 +2,11 @@ package valaevatests;
 
 import allclasses.valaeva.Cat;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-public class cattests {
+public class Cattests {
 
     @Test
     public void testCatCreation() {
