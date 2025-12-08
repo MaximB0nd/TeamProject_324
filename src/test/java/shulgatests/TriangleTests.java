bc4b126897e19b2d123aa5339deff397b4c22383
sqtwix <ivan20140767@gmail.com>
2025-12-08 16:39:42 +0300

    test: tests with allure

--- src/test/java/shulgatests/TriangleTests.java
+++ src/test/java/shulgatests/TriangleTests.java
@@ -1,12 +1,16 @@
 package shulgatests;
 
 import allclasses.shulga.Triangle;
+import io.qameta.allure.Description;
 import org.junit.Assert;
 import org.junit.Test;
+import io.qameta.allure.junit4.DisplayName;
 
 public class TriangleTests {
 
+
     @Test
+    @DisplayName("Проверка на корректное создание труегольника и на правильное значение сторон")
     public void testCreateTriangle() {
         Triangle triangle = new Triangle(3, 4, 5);
         Assert.assertNotNull(triangle);
@@ -16,6 +20,7 @@ public class TriangleTests {
     }
 
     @Test
+    @DisplayName("Проверка метода, определяющего сущестыцует треугольник или нет")
     public void testIsExist() {
         Triangle validTriangle = new Triangle(3, 4, 5);
         Triangle invalidTriangle = new Triangle(1, 1, 3);
@@ -25,6 +30,7 @@ public class TriangleTests {
     }
 
     @Test
+    @DisplayName("Проверка нахождения периметра")
     public void testGetPerimeter() {
         Triangle triangle = new Triangle(3, 4, 5);
         double expected = 12.0;
@@ -32,6 +38,7 @@ public class TriangleTests {
     }
 
     @Test
+    @DisplayName("Проверка нахождения площади")
     public void testGetSquare() {
         Triangle triangle = new Triangle(3, 4, 5);
         double expected = 6.0;
@@ -39,6 +46,7 @@ public class TriangleTests {
     }
 
     @Test
+    @DisplayName("Проверка нахождения площади и периметра у равностороннего треугольника")
     public void testEquilateralTriangle() {
         Triangle equilateral = new Triangle(5, 5, 5);
         Assert.assertTrue(equilateral.IsExist());
@@ -47,12 +55,14 @@ public class TriangleTests {
     }
 
     @Test
+    @DisplayName("Проверка создания труегольника с нулевой стороной")
     public void testZeroSideTriangle() {
         Triangle zeroSide = new Triangle(0, 4, 5);
         Assert.assertFalse(zeroSide.IsExist());
     }
 
     @Test
+    @DisplayName("Проверка создания треугольника с отрицательной стороной")
     public void testNegativeSide() {
         Triangle negativeSide = new Triangle(-3, 4, 5);
         Assert.assertFalse(negativeSide.IsExist());
