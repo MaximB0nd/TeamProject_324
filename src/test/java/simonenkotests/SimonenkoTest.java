1d889a1f7c6b004d94698dedc37c4ab64d0b068b
simka <sonyasimonenko20093@gmail.com>
2025-12-08 16:35:55 +0300

    test: test with Allure

--- src/test/java/simonenkotests/SimonenkoTest.java
+++ src/test/java/simonenkotests/SimonenkoTest.java
@@ -1,16 +1,14 @@
 package simonenkotests;
 
-import org.junit.Assert;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
 import allclasses.simonenko.Fractions;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
-public class test_simonenko {
+public class SimonenkoTest {
     @Test
+    @DisplayName("Сложение: 1/2 + 1/3 = 5/6")
     public void testAdd() {
         Fractions a = new Fractions(1, 2);
         Fractions b = new Fractions(1, 3);
@@ -18,6 +16,7 @@ public class test_simonenko {
         assertEquals("5/6", result.toString());
     }
     @Test
+    @DisplayName("Вычитание: 1/2 - 1/3 = 1/6")
     public void testSubtract() {
         Fractions a = new Fractions(1, 2);
         Fractions b = new Fractions(1, 3);
@@ -25,16 +24,19 @@ public class test_simonenko {
         assertEquals("1/6", result.toString());
     }
     @Test
+    @DisplayName("Целое число как дробь: 5 = '5'")
     public void testWholeNumber() {
         Fractions frac = new Fractions(5);
         assertEquals("5", frac.toString());
     }
     @Test
+    @DisplayName("Создание дроби: 1/2 = '1/2'")
     public void testConstructor() {
         Fractions frac = new Fractions(1, 2);
         assertEquals("1/2", frac.toString());
     }
     @Test
+    @DisplayName("Исключение при знаменателе = 0")
     public void testConstructorWithZeroDenominator() {
         assertThrows(IllegalArgumentException.class, () -> {
             new Fractions(1, 0);
